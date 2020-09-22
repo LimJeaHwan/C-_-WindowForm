87735fd654baa9ae8759da98bd2184b226ab24a7
LimJeaHwan <dlawoghks999@naver.com>
2020-09-23 02:26:02 +0900

    4주차 실습

---
+++ 4Week/B_Progress/B_Progress/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B_Progress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Num = 0; //진행 숫자
+        string OrgStr = ""; //결과저장
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = false;
+        }
+
+        private void btnReStart_Click(object sender, EventArgs e)
+        {
+            Num = 0;
+            this.Timer.Enabled = true;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Num++;
+            if(Num>100)
+            {
+                this.Timer.Enabled = false;
+                return;
+            }
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr + Num.ToString() + "%";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OrgStr = this.lblStatus.Text;
+        }
+
+       
+    }
+
+}
