9e949d3992ce824756d91a692912cc6e77d89644
LimJeaHwan <dlawoghks999@naver.com>
2020-11-10 13:21:21 +0900

    10주차 파일정보 저장하기

--- 10Week/B_FileExcelSave/B_FileExcelSave/Form1.Designer.cs
+++ 10Week/B_FileExcelSave/B_FileExcelSave/Form1.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.gbSave = new System.Windows.Forms.GroupBox();
+            this.rbExcel = new System.Windows.Forms.RadioButton();
+            this.rbText = new System.Windows.Forms.RadioButton();
             this.btnPath = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.rbText = new System.Windows.Forms.RadioButton();
-            this.rbExcel = new System.Windows.Forms.RadioButton();
             this.lvFile = new System.Windows.Forms.ListView();
-            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
-            this.sfdFile = new System.Windows.Forms.SaveFileDialog();
             this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.shSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdFile = new System.Windows.Forms.SaveFileDialog();
             this.gbSave.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,23 +55,16 @@
             this.gbSave.TabStop = false;
             this.gbSave.Text = "파일 저장";
             // 
-            // btnPath
+            // rbExcel
             // 
-            this.btnPath.Location = new System.Drawing.Point(58, 34);
-            this.btnPath.Name = "btnPath";
-            this.btnPath.Size = new System.Drawing.Size(81, 62);
-            this.btnPath.TabIndex = 0;
-            this.btnPath.Text = "파일";
-            this.btnPath.UseVisualStyleBackColor = true;
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(522, 34);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(79, 62);
-            this.btnSave.TabIndex = 1;
-            this.btnSave.Text = "저장";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.rbExcel.AutoSize = true;
+            this.rbExcel.Location = new System.Drawing.Point(183, 23);
+            this.rbExcel.Name = "rbExcel";
+            this.rbExcel.Size = new System.Drawing.Size(63, 16);
+            this.rbExcel.TabIndex = 1;
+            this.rbExcel.TabStop = true;
+            this.rbExcel.Text = "EXCEL";
+            this.rbExcel.UseVisualStyleBackColor = true;
             // 
             // rbText
             // 
@@ -84,16 +77,25 @@
             this.rbText.Text = "TXT";
             this.rbText.UseVisualStyleBackColor = true;
             // 
-            // rbExcel
+            // btnPath
             // 
-            this.rbExcel.AutoSize = true;
-            this.rbExcel.Location = new System.Drawing.Point(183, 23);
-            this.rbExcel.Name = "rbExcel";
-            this.rbExcel.Size = new System.Drawing.Size(63, 16);
-            this.rbExcel.TabIndex = 1;
-            this.rbExcel.TabStop = true;
-            this.rbExcel.Text = "EXCEL";
-            this.rbExcel.UseVisualStyleBackColor = true;
+            this.btnPath.Location = new System.Drawing.Point(58, 34);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(81, 62);
+            this.btnPath.TabIndex = 0;
+            this.btnPath.Text = "파일";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(522, 34);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(79, 62);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // lvFile
             // 
@@ -112,12 +114,6 @@
             this.lvFile.TabIndex = 2;
             this.lvFile.UseCompatibleStateImageBehavior = false;
             this.lvFile.View = System.Windows.Forms.View.Details;
-            // 
-            // ofdFile
-            // 
-            this.ofdFile.FileName = "openFileDialog1";
-            this.ofdFile.Filter = "모든 파일(*.*)|*.*";
-            this.ofdFile.Multiselect = true;
             // 
             // chName
             // 
@@ -143,6 +139,12 @@
             // 
             this.chPath.Text = "경로";
             this.chPath.Width = 250;
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.Filter = "모든 파일(*.*)|*.*";
+            this.ofdFile.Multiselect = true;
             // 
             // Form1
             // 
